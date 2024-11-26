c017b8d6a1f92a793b867e4f484064c907ee7461
Johan Schuijt <johan.schuijt@ftm.nl>
2024-11-26 12:27:19 +0100

    scrape single page of requests

--- scrape.py
+++ scrape.py
@@ -1,12 +1,29 @@
 import requests
+from datetime import datetime, timedelta
 from lxml.html import fromstring
 
-params = {
-    "query": "variety:sent",
-    "request_date_after": "2024/09/01",
-    "request_date_before": "2024/09/30",
-}
 
-response = requests.get("https://www.asktheeu.org/en/list/all", params=params)
+# AskTheEU for some reason limits their pages to max 20. To fetch all requests
+# we fetch per month to not exceed this arbitrary limit
+def scrape_index(after: datetime, before: datetime):
+    params = {
+        "query": "variety:sent",
+        "request_date_after": after.strftime("%Y/%m/%d"),
+        "request_date_before": before.strftime("%Y/%m/%d"),
+        "page": 1,
+    }
 
-print(response.text)
+    response = requests.get("https://www.asktheeu.org/en/list/all", params=params)
+
+    doc = fromstring(response.text)
+
+    for element in doc.findall(
+        './/div[@class="request_listing"]//span[@class="head"]/a'
+    ):
+        yield element.get("href")
+
+
+after = datetime.now().replace(day=1)
+before = after.replace(month=after.month + 1)
+
+print(list(scrape_index(after, before)))
