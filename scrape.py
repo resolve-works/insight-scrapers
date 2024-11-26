7aa36974c54e4f655ca6fb40427c90912b82d062
Johan Schuijt <johan.schuijt@ftm.nl>
2024-11-26 12:55:35 +0100

    scrape index by scraping all pages of dateranges

--- scrape.py
+++ scrape.py
@@ -1,11 +1,15 @@
 import requests
-from datetime import datetime, timedelta
+import logging
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from lxml.html import fromstring
+
+logging.basicConfig(level=logging.INFO)
 
 
 # AskTheEU for some reason limits their pages to max 20. To fetch all requests
-# we fetch per month to not exceed this arbitrary limit
-def scrape_index(after: datetime, before: datetime):
+# we limit to a date range to not exceed this arbitrary limit
+def scrape_index_date_range(after: datetime, before: datetime):
     params = {
         "query": "variety:sent",
         "request_date_after": after.strftime("%Y/%m/%d"),
@@ -13,17 +17,41 @@ def scrape_index(after: datetime, before: datetime):
         "page": 1,
     }
 
-    response = requests.get("https://www.asktheeu.org/en/list/all", params=params)
+    while True:
+        logging.info(
+            f"Scraping index {params['request_date_after']} to {params['request_date_before']} page {params['page']}"
+        )
+        response = requests.get("https://www.asktheeu.org/en/list/all", params=params)
 
-    doc = fromstring(response.text)
+        doc = fromstring(response.text)
 
-    for element in doc.findall(
-        './/div[@class="request_listing"]//span[@class="head"]/a'
-    ):
-        yield element.get("href")
+        elements = doc.findall(
+            './/div[@class="request_listing"]//span[@class="head"]/a'
+        )
+
+        if len(elements) < 1:
+            break
+
+        for element in elements:
+            yield element.get("href")
+
+        params["page"] += 1
 
 
-after = datetime.now().replace(day=1)
-before = after.replace(month=after.month + 1)
+def scrape_index():
+    after = datetime.now().replace(day=1)
 
-print(list(scrape_index(after, before)))
+    while True:
+        if after.year < 2024:
+            break
+
+        before = after.replace(month=after.month + 1)
+        for href in scrape_index_date_range(after, before):
+            yield href
+
+        after -= relativedelta(months=1)
+
+
+hrefs = list(scrape_index())
+
+print(hrefs)
